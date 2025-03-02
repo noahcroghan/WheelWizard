4ea11494db4d03bea41d6d4ad7ecb4c986f10382
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-03-02 01:57:50 +0100

    most pain i have ever had making components

---
+++ WheelWizard/Views/Components/WhWzLibrary/Badge.axaml.cs
@@ -0,0 +1,49 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using System;
+
+namespace WheelWizard.Views.Components.WhWzLibrary;
+
+public class Badge : TemplatedControl
+{
+    public enum BadgeVariant
+    {
+        None, // None is the default and should be the first enum value
+        WhWzDev,
+        RrDev,
+        GoldWinner,
+        SilverWinner,
+        BronzeWinner
+    }
+    
+    public static readonly StyledProperty<BadgeVariant> VariantProperty =
+        AvaloniaProperty.Register<Badge, BadgeVariant>(nameof(Variant));
+
+    public BadgeVariant Variant
+    {
+        get => GetValue(VariantProperty);
+        set => SetValue(VariantProperty, value);
+    }
+    
+    private void UpdateStyleClasses(BadgeVariant variant)
+    {
+        var types = Enum.GetValues<BadgeVariant>();
+        foreach (var enumType in types)
+        {
+            Classes.Remove(enumType.ToString());
+        }
+        Console.WriteLine("Variant + " + variant);
+        Classes.Add(variant.ToString());
+    }
+    
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == VariantProperty)
+            UpdateStyleClasses(change.GetNewValue<BadgeVariant>());
+    }
+}
+
