5bf8561d7e8fa68683327e0fca5560f80b740ea5
Ruben <a.r.c.broere@students.uu.nl>
2025-03-08 14:38:48 +0100

    fix: usings

--- WheelWizard/Views/App.axaml.cs
+++ WheelWizard/Views/App.axaml.cs
@@ -1,7 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using WheelWizard.Services.UrlProtocol;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WheelWizard.Views;
 
