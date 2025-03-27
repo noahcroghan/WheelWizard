9147c3d8f560748f5ae833897e4cd495166a9135
Ruben <github@rubenbroere.com>
2025-03-27 23:52:27 +0100

    docs: add XML documentation for OperationError and OperationResult classes

--- WheelWizard/Shared/OperationResult/OperationResult.cs
+++ WheelWizard/Shared/OperationResult/OperationResult.cs
@@ -43,12 +43,33 @@ public class OperationResult
         Error = error;
     }
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="OperationResult"/> class with the specified error.
+    /// </summary>
+    /// <param name="error">The error that occurred during the operation.</param>
+    /// <returns>A new instance of the <see cref="OperationResult"/> class.</returns>
     public static OperationResult Fail(OperationError error) => new(error);
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="OperationResult"/> class that indicates success.
+    /// </summary>
+    /// <returns>A new instance of the <see cref="OperationResult"/> class.</returns>
     public static OperationResult Ok() => new();
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="OperationResult"/> class that indicates success.
+    /// </summary>
+    /// <param name="error">The error that occurred during the operation.</param>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <returns>A new instance of the <see cref="OperationResult{T}"/> class.</returns>
     public static OperationResult<T> Fail<T>(OperationError error) => new(error);
 
+    /// <summary>
+    /// Creates a new instance of the <see cref="OperationResult{T}"/> class that indicates success.
+    /// </summary>
+    /// <param name="value">The value of the operation result.</param>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <returns>A new instance of the <see cref="OperationResult{T}"/> class.</returns>
     public static OperationResult<T> Ok<T>(T value) => new(value);
 
     public static implicit operator OperationResult(OperationError error) => new(error);
--- WheelWizard/Shared/OperationResult/OperationResult`1.cs
+++ WheelWizard/Shared/OperationResult/OperationResult`1.cs
@@ -2,6 +2,10 @@
 
 namespace WheelWizard.Shared;
 
+/// <summary>
+/// Represents the result of an operation.
+/// </summary>
+/// <typeparam name="T">The type of the value.</typeparam>
 public class OperationResult<T> : OperationResult
 {
     [MemberNotNullWhen(true, nameof(Value))]
@@ -10,13 +14,24 @@ public class OperationResult<T> : OperationResult
     [MemberNotNullWhen(false, nameof(Value))]
     public override bool IsFailure => base.IsFailure;
 
+    /// <summary>
+    /// The value of the operation result.
+    /// </summary>
     public T? Value { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OperationResult{T}"/> class.
+    /// </summary>
+    /// <param name="value">The value of the operation result.</param>
     public OperationResult(T value)
     {
         Value = value;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OperationResult{T}"/> class with the specified error.
+    /// </summary>
+    /// <param name="error">The error that occurred during the operation.</param>
     public OperationResult(OperationError error) : base(error)
     {
     }
