1b265d10110b82d1fee049b38f6e7d67caeaab9f
Ruben <github@rubenbroere.com>
2025-03-28 21:05:35 +0100

    fix: update RrRoomsSingletonService to use IApiCaller interface for improved type safety

--- WheelWizard/Features/RrRooms/RrRoomsSingletonService.cs
+++ WheelWizard/Features/RrRooms/RrRoomsSingletonService.cs
@@ -7,7 +7,7 @@ public interface IRrRoomsSingletonService
     Task<OperationResult<List<RwfcRoom>>> GetRoomsAsync();
 }
 
-public class RrRoomsSingletonService(ApiCaller<IRwfcApi> apiCaller) : IRrRoomsSingletonService
+public class RrRoomsSingletonService(IApiCaller<IRwfcApi> apiCaller) : IRrRoomsSingletonService
 {
     public async Task<OperationResult<List<RwfcRoom>>> GetRoomsAsync()
     {
