41064e8235c5badcf7566fb5db7a2ec861b7808b
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-03-30 21:51:21 +0200

    fix font changes and add about page

---
+++ WheelWizard/Views/Pages/Settings/AppInfo.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace WheelWizard.Views.Pages.Settings;
+
+public partial class AppInfo : UserControl
+{
+
+    public AppInfo()
+    {
+        InitializeComponent();
+    }
+}
