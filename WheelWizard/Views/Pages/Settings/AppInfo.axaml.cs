04627a1a2efe0e12db8af1543d95140b3e113fcd
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-03-30 22:46:30 +0200

    links for the font credits

--- WheelWizard/Views/Pages/Settings/AppInfo.axaml.cs
+++ WheelWizard/Views/Pages/Settings/AppInfo.axaml.cs
@@ -1,12 +1,22 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 
 namespace WheelWizard.Views.Pages.Settings;
 
 public partial class AppInfo : UserControl
 {
-
     public AppInfo()
     {
         InitializeComponent();
+    }
+
+    private void OpenLick_OnClick(object? sender, EventArgs e)
+    {
+        if (sender is not TemplatedControl control)
+            return;
+        if (control.Tag == null)
+            return;
+        
+        ViewUtils.OpenLink(control.Tag.ToString()!);
     }
 }
