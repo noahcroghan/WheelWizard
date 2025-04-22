25563b7fa1d8690595be24ffab527167278e31f9
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-04-22 22:42:57 +0200

    start

---
+++ WheelWizard/Views/Popups/MiiManagement/MiiEditor/StartPage.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WheelWizard.Views.Popups.MiiManagement.MiiEditor;
+
+public partial class StartPage : UserControl
+{
+    public StartPage()
+    {
+        InitializeComponent();
+    }
+}
+
