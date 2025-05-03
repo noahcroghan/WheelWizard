6a1b7848dbe06e62fe1caccddd7fc075383ae991
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-05-03 15:28:14 +0200

    making sure only 1 image requests happen when duplicated miis

--- WheelWizard/Features/MiiImages/Domain/MiiImageSpecifications.cs
+++ WheelWizard/Features/MiiImages/Domain/MiiImageSpecifications.cs
@@ -23,7 +23,11 @@ public class MiiImageSpecifications
     public override string ToString()
     {
         // If we put all the things in this string, then the Key at least is unique
-        return $"{Name}_{Size}{Expression}{Type}_{BackgroundColor}{InstanceCount}_{CharacterRotate}{CameraRotate}_{CachePriority}";
+        var parts = $"{Name}_{Size}{Expression}{Type}";
+        parts += $"{BackgroundColor}{InstanceCount}";
+        parts += $"{CharacterRotate}{CameraRotate}";
+        parts += $"{CachePriority}";
+        return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(parts));
     }
 
     #region Enums
@@ -32,12 +36,24 @@ public class MiiImageSpecifications
     public enum FaceExpression
     {
         normal,
+        normal_open_mouth,
         smile,
+        smile_open_mouth,
         frustrated,
         anger,
+        anger_open_mouth,
         blink,
+        blink_open_mouth,
         sorrow,
+        sorrow_open_mouth,
         surprise,
+        surprise_open_mouth,
+        wink_right,
+        wink_left,
+        like_wink_left,
+        like_wink_right,
+        wink_left_open_mouth,
+        wink_right_open_mouth,
     }
 
     public enum ImageSize
@@ -49,6 +65,7 @@ public class MiiImageSpecifications
     public enum BodyType
     {
         face,
+        face_only,
         all_body,
     }
 
