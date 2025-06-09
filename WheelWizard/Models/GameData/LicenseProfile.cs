296148309e2dfa8ee29a461d63342a87274f5715
patchzyy <64382339+patchzyy@users.noreply.github.com>
2025-06-09 17:54:52 +0200

    Statistics

--- WheelWizard/Models/GameData/LicenseProfile.cs
+++ WheelWizard/Models/GameData/LicenseProfile.cs
@@ -5,4 +5,5 @@ public class LicenseProfile : PlayerProfileBase
     public required uint TotalRaceCount { get; set; }
     public required uint TotalWinCount { get; set; }
     public List<FriendProfile> Friends { get; set; } = [];
+    public Statistics Statistics { get; set; }
 }
