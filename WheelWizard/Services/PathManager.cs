056d52490cf7e6365c7a39c2976b10a61fff62fd
Patchzy <64382339+patchzyy@users.noreply.github.com>
2025-09-22 22:57:08 +0200

    Update PathManager.cs

--- WheelWizard/Services/PathManager.cs
+++ WheelWizard/Services/PathManager.cs
@@ -41,9 +41,7 @@ public static class PathManager
     private static string AppDataFolder => Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
     private static string LocalAppDataFolder => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
     private static string UnixAppDataOverrideFilePath =>
-        OperatingSystem.IsMacOS()
-            ? Path.Combine(HomeFolderPath, "Library", "Preferences", "com.wheelwizard.appdata-location")
-            : Path.Combine(HomeFolderPath, ".config", "wheelwizard-appdata-location");
+        Path.Combine(AppDataFolder, "wheelwizard-appdata-location");
 
     // Wheel wizard's appdata paths (don't have to be expressions since they don't depend on user input like the others)
     public static string WheelWizardAppdataPath
